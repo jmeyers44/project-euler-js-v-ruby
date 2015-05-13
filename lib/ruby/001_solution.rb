60ee1b28bc538e33be9ab854433c621d5bfbab82
Jonathan Meyers <jmeyers44@gmail.com>
2015-05-13 09:47:28 -0400

    add first solutions in ruby and js

---
+++ lib/ruby/001_solution.rb
@@ -0,0 +1,12 @@
+require 'benchmark'
+total = 0
+execution_time = Benchmark.realtime{  
+  x = 3
+  until x >= 100000000
+    if x % 3 == 0 || x % 5 == 0
+      total += x
+    end
+    x += 1
+  end}
+puts "The sum is #{total}"
+puts "Execution Time: #{"%1.12f" % execution_time} seconds"
